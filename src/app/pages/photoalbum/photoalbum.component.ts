f011511ca704648257c7d00766a655f4a79cfc5d
blessie.balagtas <blessie.balagtas@pointwest.com.ph>
2023-11-20 14:30:27 +0800

    Added new components and edited home and profile page

---
+++ src/app/pages/photoalbum/photoalbum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photoalbum',
+  templateUrl: './photoalbum.component.html',
+  styleUrls: ['./photoalbum.component.css']
+})
+export class PhotoalbumComponent {
+
+}
