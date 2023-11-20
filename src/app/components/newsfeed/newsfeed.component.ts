2f6bb37fc067a99b509d8c1df2dd8a1e8d9756fc
blessie.balagtas <blessie.balagtas@pointwest.com.ph>
2023-11-20 17:07:55 +0800

    Just update some part of home

--- src/app/components/newsfeed/newsfeed.component.ts
+++ src/app/components/newsfeed/newsfeed.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./newsfeed.component.css']
 })
 export class NewsfeedComponent {
+  posts = [
+    { id: 1, content: 'This is a sample post.', likes: 0, showComments: false, newComment: '', comments: [] }
+    // Add more posts as needed
+  ];
 
+  toggleLike(post: any): void {
+    post.likes = post.likes + (post.liked ? -1 : 1);
+    post.liked = !post.liked;
+  }
+
+  showComments(post: any): void {
+    post.showComments = !post.showComments;
+  }
+
+  addComment(post: any): void {
+    const commentText = post.newComment.trim();
+
+    if (commentText !== '') {
+      post.comments.push(commentText);
+      post.newComment = '';
+    }
+  }
 }
