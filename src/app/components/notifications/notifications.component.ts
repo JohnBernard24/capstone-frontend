86bb0311693cf4645a85de0401baeac813dc5da2
timothy.dy <timothy.dy@pointwest.com.ph>
2023-11-21 10:54:11 +0800

    Add notifications

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+// notification.component.ts
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.css']
+    selector: 'app-notifications',
+    templateUrl: './notifications.component.html',
+    styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit {
+    notifications: any[] = [
+        { sender: 'Blessie Balagtas', action: "liked", type: 'photo' },
+        { sender: 'John Bernard Tinio', action: "commented on", type: 'post' },
+        { sender: 'Jigs Capinpuyan', action: "liked", type: 'post' }
+        // Add more notifications as needed
+    ];
 
+    ngOnInit() {
+        // Initialize or fetch notifications here
+    }
+
+    markAsRead(notification: any) {
+        // Perform actions to mark the notification as read
+        // For example, remove it from the list
+        const index = this.notifications.indexOf(notification);
+        if (index !== -1) {
+            this.notifications.splice(index, 1);
+        }
+    }
+
+    deleteAllNotifications() {
+        // Perform actions to delete all notifications
+        this.notifications = [];
+    }
 }
