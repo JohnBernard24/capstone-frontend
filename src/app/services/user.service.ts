0a994b75962a68c7cad4498f6485a558569ee57d
JohnBernard24 <147012108+JohnBernard24@users.noreply.github.com>
2023-11-22 11:57:49 +0800

    Connected email verification from frontend

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -23,10 +23,10 @@ export class UserService {
     return this.http.post(this.baseUrl + '/login', {email, password})
   }
 
-  sendEmail(userId: number, recipientEmail: string): Observable<Object>{
-    const body = {recipientEmail};
-    return this.http.get(`${this.baseUrl}/verify-email/${userId}`, {params: body});
+  sendEmail(userId: number, recipientEmail: string): Observable<Object> {
+    return this.http.post(this.baseUrl + `/verify-email`, {userId, recipientEmail});
   }
+
 
 
   //USER PROFILE
