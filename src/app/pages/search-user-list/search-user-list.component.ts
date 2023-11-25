aef827a472675dfb7ff39838d17b58482345585a
wenceslao14 <wenceslao.vinzons@pointwest.com.ph>
2023-11-25 16:35:29 +0800

    Added search option and user-list page

---
+++ src/app/pages/search-user-list/search-user-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-user-list',
+  templateUrl: './search-user-list.component.html',
+  styleUrls: ['./search-user-list.component.css']
+})
+export class SearchUserListComponent {
+
+}
