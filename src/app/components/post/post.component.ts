7538faa950ecbcf6c307569ee172f94ac89bb8fb
timothy.dy <timothy.dy@pointwest.com.ph>
2023-11-28 11:37:34 +0800

    Added comments modal

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -13,6 +13,12 @@ export class PostComponent implements OnInit{
   userId: number = Number(this.sessionService.getUserId());
 
   likedUsers = ['Ad Min', 'Jayvee Tinio', 'Blessie Balagtas', 'Min Ad'];
+  comments: any[] = [
+    { user: 'Ad Min', text: 'If the sentence is the essential soul to express oneself in their own way, then the paragraph is the virtual body of it. The Text Generator is an intelligent tool that creates random text incorporated with random thoughts. This smart tool is a virtual friend of yours that can talk to you in multidimensional thinking. It will provide you with thoughts, concepts, and ideas of different topics that will not only assist you in creating new knowledge but also enhance your brain function.' },
+    { user: 'Jayvee Tinio', text: 'Amazing' },
+    { user: 'Blessie Balagtas', text: 'Slay!' },
+    { user: 'Min Ad', text: 'Lorem ipsum et dolor sit amet.' }
+  ];
   
   // formattedDate: string | undefined = "";
   
